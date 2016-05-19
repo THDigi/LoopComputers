a92ef628882cca34f54132a33b16779c300cd760
Digi <hunter_digital_sl@yahoo.com>
2016-05-19 23:40:50 +0300

    Updated, added terminal interface
    - Added slider to control the repeat delay in the terminal UI.
    - Replaced name storage with something more universal, has backwards
    compatibility.

--- Data/Scripts/LoopComputers/LoopComputers.cs
+++ Data/Scripts/LoopComputers/LoopComputers.cs
@@ -1,19 +1,21 @@
 using System;
 using System.Linq;
-using Sandbox.Common;
+using System.Text;
+using System.Text.RegularExpressions;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.ModAPI;
-using VRage.Game;
+using Sandbox.ModAPI.Interfaces.Terminal;
 using VRage.Game.Components;
 using VRage.ModAPI;
 using VRage.ObjectBuilders;
 using VRage.Utils;
 using Digi.Utils;
 using VRageMath;
+using Ingame = Sandbox.ModAPI.Ingame;
 
 namespace Digi.LoopComputers
 {
-    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
     public class LoopComputersMod : MySessionComponentBase
     {
         public static bool init = false;
@@ -24,6 +26,29 @@ namespace Digi.LoopComputers
             
             Log.Init();
             Log.Info("Initialized.");
+            
+            var tc = MyAPIGateway.TerminalControls;
+            
+            tc.AddControl<Ingame.IMyProgrammableBlock>(tc.CreateControl<IMyTerminalControlSeparator, Ingame.IMyProgrammableBlock>(string.Empty));
+            
+            var c = tc.CreateControl<IMyTerminalControlSlider, Ingame.IMyProgrammableBlock>("Digi.LoopComputers.RepeatTime");
+            c.Title = MyStringId.GetOrCompute("Self run:");
+            c.Tooltip = MyStringId.GetOrCompute("The block runs itself at the specified interval.\nValues smaller than 0.016s (one tick) are considered off.");
+            c.SupportsMultipleBlocks = true;
+            c.SetLogLimits(0.015f, 600f);
+            c.Setter = (b, v) => b.GameLogic.GetAs<LoopPB>().DelayTime = (v < 0.016f ? 0 : v);
+            c.Getter = (b) => (b.GameLogic.GetAs<LoopPB>().DelayTime);
+            c.Writer = delegate(IMyTerminalBlock b, StringBuilder s)
+            {
+                float v = b.GameLogic.GetAs<LoopPB>().DelayTime;
+                
+                if(v < 0.016f)
+                    s.Append("Off");
+                else
+                    s.AppendFormat("{0:0.000}s / {1}ticks", v, Math.Round(v * 60));
+            };
+            
+            tc.AddControl<Ingame.IMyProgrammableBlock>(c);
         }
         
         protected override void UnloadData()
@@ -44,13 +69,13 @@ namespace Digi.LoopComputers
             Log.Close();
         }
         
-        public override void UpdateAfterSimulation()
+        public override void UpdateBeforeSimulation()
         {
             try
             {
                 if(!init)
                 {
-                    if(MyAPIGateway.Multiplayer == null || !MyAPIGateway.Multiplayer.IsServer)
+                    if(MyAPIGateway.Session == null || MyAPIGateway.Multiplayer == null)
                         return;
                     
                     Init();
@@ -67,13 +92,17 @@ namespace Digi.LoopComputers
     public class LoopPB : MyGameLogicComponent
     {
         private bool first = true;
-        private int delayTicks = 0;
+        private float delayTime = 0;
         private int tick = 0;
+        private byte propertiesChangedDelay = 0;
         
-        private const string TAG_START = "[repeat";
-        private const string TAG_END = "]";
-        private const int MIN_TICKS = 10;
-        private const int MAX_TICKS = 60*60*10;
+        private static readonly StringBuilder str = new StringBuilder();
+        
+        private const string DATA_TAG_START = "{LoopComputers:";
+        private const char DATA_TAG_END = '}';
+        
+        private const string LEGACY_TAG_START = "[repeat";
+        private const string LEGACY_TAG_END = "]";
         
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
@@ -89,6 +118,7 @@ namespace Digi.LoopComputers
         {
             var block = Entity as IMyTerminalBlock;
             block.CustomNameChanged += NameChanged;
+            ReadLegacyName(block);
             NameChanged(block);
         }
         
@@ -98,40 +128,78 @@ namespace Digi.LoopComputers
             block.CustomNameChanged -= NameChanged;
         }
         
-        public void NameChanged(IMyTerminalBlock b)
+        public float DelayTime
+        {
+            get { return delayTime; }
+            set
+            {
+                delayTime = (float)Math.Round(value, 3);
+                
+                if(propertiesChangedDelay <= 0)
+                    propertiesChangedDelay = 30;
+            }
+        }
+        
+        public void NameChanged(IMyTerminalBlock block)
         {
             try
             {
-                var block = b as IMyFunctionalBlock;
-                var name = block.CustomName.ToLower();
-                delayTicks = 0;
+                delayTime = 0;
                 
-                if(name.Contains(TAG_START + TAG_END))
-                {
-                    delayTicks = 60;
-                }
-                else if(name.Contains(TAG_START))
-                {
-                    int startIndex = name.IndexOf(TAG_START) + TAG_START.Length;
-                    int endIndex = name.IndexOf(TAG_END, startIndex);
-                    
-                    if(startIndex == -1 || endIndex == -1)
-                    {
-                        delayTicks = 0;
-                        return;
-                    }
-                    
-                    string arg = name.Substring(startIndex, endIndex - startIndex).Trim(' ', ':', '=');
-                    double delay;
-                    
-                    if(double.TryParse(arg, out delay))
-                        delayTicks = (int)MathHelper.Clamp(delay * 60, MIN_TICKS, MAX_TICKS);
-                }
+                var name = block.CustomName.ToLower();
+                var startIndex = name.IndexOf(DATA_TAG_START, StringComparison.OrdinalIgnoreCase);
+                
+                if(startIndex == -1)
+                    return;
+                
+                startIndex += DATA_TAG_START.Length;
+                var endIndex = name.IndexOf(DATA_TAG_END, startIndex);
+                
+                if(endIndex == -1)
+                    return;
+                
+                var data = name.Substring(startIndex, (endIndex - startIndex));
+                delayTime = (float)Math.Round(float.Parse(data), 3);
             }
             catch(Exception e)
             {
                 Log.Error(e);
             }
+        }
+        
+        private void SaveToName(string forceName = null)
+        {
+            var block = Entity as IMyTerminalBlock;
+            str.Clear();
+            str.Append(forceName ?? GetNameNoData().Trim());
+            
+            if(delayTime > 0)
+            {
+                str.Append(' ', 3);
+                str.Append(DATA_TAG_START);
+                str.Append(delayTime);
+                str.Append(DATA_TAG_END);
+            }
+            
+            block.SetCustomName(str.ToString());
+        }
+        
+        private string GetNameNoData()
+        {
+            var block = Entity as IMyTerminalBlock;
+            var name = block.CustomName;
+            var startIndex = name.IndexOf(DATA_TAG_START, StringComparison.OrdinalIgnoreCase);
+            
+            if(startIndex == -1)
+                return name;
+            
+            var nameNoData = name.Substring(0, startIndex);
+            var endIndex = name.IndexOf(DATA_TAG_END, startIndex);
+            
+            if(endIndex == -1)
+                return nameNoData;
+            else
+                return nameNoData + name.Substring(endIndex + 1);
         }
         
         public override void UpdateBeforeSimulation()
@@ -148,12 +216,20 @@ namespace Digi.LoopComputers
                     return;
                 }
                 
-                if(delayTicks == 0)
+                if(!MyAPIGateway.Multiplayer.IsServer)
+                    return;
+                
+                if(propertiesChangedDelay > 0 && --propertiesChangedDelay == 0)
+                {
+                    SaveToName();
+                }
+                
+                if(Math.Abs(delayTime) < float.Epsilon)
                     return;
                 
                 var block = Entity as IMyFunctionalBlock;
                 
-                if(block.Enabled && block.IsWorking && block.IsFunctional && ++tick >= delayTicks)
+                if(block.Enabled && block.IsWorking && block.IsFunctional && ++tick >= (delayTime * 60))
                 {
                     tick = 0;
                     block.GetActionWithName("Run").Apply(block);
@@ -163,6 +239,36 @@ namespace Digi.LoopComputers
             {
                 Log.Error(e);
             }
+        }
+        
+        private void ReadLegacyName(IMyTerminalBlock block)
+        {
+            var name = block.CustomName.ToLower();
+            delayTime = 0;
+            
+            if(name.Contains(LEGACY_TAG_START + LEGACY_TAG_END))
+            {
+                delayTime = 1;
+            }
+            else if(name.Contains(LEGACY_TAG_START))
+            {
+                int startIndex = name.IndexOf(LEGACY_TAG_START, StringComparison.Ordinal) + LEGACY_TAG_START.Length;
+                int endIndex = name.IndexOf(LEGACY_TAG_END, startIndex, StringComparison.Ordinal);
+                
+                if(startIndex == -1 || endIndex == -1)
+                {
+                    delayTime = 0;
+                    return;
+                }
+                
+                string arg = name.Substring(startIndex, endIndex - startIndex).Trim(' ', ':', '=');
+                float delay;
+                
+                if(float.TryParse(arg, out delay))
+                    delayTime = MathHelper.Clamp(delay, 0.016f, 600);
+            }
+            
+            SaveToName(Regex.Replace(block.CustomName, @"\[repeat([\s\:\=][\d.]+|)\]", "", RegexOptions.IgnoreCase).Trim());
         }
     }
 }
